f00316ec192b5288dc37796ec837ba10e32f0c5e
Bruno Oliveira <oliveiradantas96@gmail.com>
2017-04-27 18:29:31 -0300

    [FIELD VALIDATION AND FORMATTING STRINGS] Field validation of LoginActivity, SignupActivity, ResetPassawordActivity, MainActivity and CreateActivity. And Formatting Strings.

--- app/src/main/java/com/example/arthur/redcup/View/ResetPasswordActivity.java
+++ app/src/main/java/com/example/arthur/redcup/View/ResetPasswordActivity.java
@@ -57,7 +57,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 String email = inputEmail.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    inputEmail.setError(getString(R.string.registered_email));
                     return;
                 }
 
@@ -67,9 +67,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(ResetPasswordActivity.this, getString(R.string.send_instructions), Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(ResetPasswordActivity.this, getString(R.string.send_failed), Toast.LENGTH_SHORT).show();
                                 }
 
                                 progressBar.setVisibility(View.GONE);
